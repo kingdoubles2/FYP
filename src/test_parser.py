53789e17a9ed40a7d09b379a53a9d40d8e6a6532
wangc9 <chunyang.wang9@mail.dcu.ie>
2026-02-26 12:19:01 +0000

    change path of stored specs

--- src/test_parser.py
+++ src/test_parser.py
@@ -2,7 +2,7 @@ from spec_parser.parser import parse_openapi
 
 
 if __name__ == "__main__":
-    with open("specs/petstore.json", "r", encoding="utf-8") as f:
+    with open("test_uploads/user1/petstore.json", "r", encoding="utf-8") as f:
         spec_text = f.read()
 
     parsed = parse_openapi(spec_text)
